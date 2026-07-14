d224eccb68fddbd9ed7d4de2e5a7791ab324e5b0
muhammedshamil8 <muhammedshamil008@gmail.com>
2026-07-15 04:58:57 +0530

    add auth and user endpoint handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/muhammedshamil8/url-shortener/internal/auth"
 	"github.com/muhammedshamil8/url-shortener/internal/config"
 	"github.com/muhammedshamil8/url-shortener/internal/models"
 	"github.com/muhammedshamil8/url-shortener/internal/response"
@@ -177,94 +176,4 @@ func (h *Handler) ListAllHandler(c *gin.Context) {
 	response.OK(c, gin.H{
 		"urls": urls,
 	})
-}
-
-// Register godoc
-//
-//	@Summary	Register a new account
-//	@Description	Register a new account
-//	@Tags	Users
-//	@Param	request	body	models.RegisterRequest	true	"Request body"
-//	@Produce	json
-//	@Success	200	{object}	models.SuccessResponse
-//	@Failure	400	{object}	models.ErrorResponse
-//	@Failure	500	{object}	models.ErrorResponse
-//	@Router	/register [post]
-func (h *Handler) RegisterHandler(c *gin.Context) {
-	var req models.RegisterRequest
-	if err := c.BindJSON(&req); err != nil {
-		response.BadRequest(c, "Invalid request body")
-		return
-	}
-
-	hashedPassword, err := auth.HashPassword(req.Password)
-	if err != nil {
-		response.InternalServerError(c, "Failed to hash password")
-		return
-	}
-
-	id, err := h.repo.CreateUser(req.Username, req.Email, hashedPassword)
-	if err != nil {
-		response.InternalServerError(c, "Failed to create user")
-		return
-	}
-	response.OK(c, gin.H{
-		"id":       id,
-		"username": req.Username,
-		"email":    req.Email,
-	})
-}
-
-// Login godoc
-//
-//	@Summary	Login to account
-//	@Description	Login to account
-//	@Tags	Users
-//	@Param	request	body	models.LoginRequest	true	"Request body"
-//	@Produce	json
-//	@Success	200	{object}	models.SuccessResponse
-//	@Failure	400	{object}	models.ErrorResponse
-//	@Failure	401	{object}	models.ErrorResponse
-//	@Failure	500	{object}	models.ErrorResponse
-//	@Router	/login [post]
-func (h *Handler) LoginHandler(c *gin.Context) {
-	var req models.LoginRequest
-	if err := c.BindJSON(&req); err != nil {
-		response.BadRequest(c, "Invalid request body")
-		return
-	}
-
-	user, err := h.repo.GetUserByEmail(req.Email)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			response.Unauthorized(c, "Invalid credentials")
-			return
-		}
-		response.InternalServerError(c, "Database error")
-		return
-	}
-
-	if err := auth.CheckPasswordHash(req.Password, user.PasswordHash); err != nil {
-		response.Unauthorized(c, "Invalid credentials")
-		return
-	}
-
-	accessToken, err := auth.GenerateToken(user.ID, user.Email, h.cfg.JWT.AccessTokenSecret, h.cfg.JWT.AccessTokenExpiry)
-	if err != nil {
-		response.InternalServerError(c, "Failed to generate access token")
-		return
-	}
-
-	refreshToken, err := auth.GenerateToken(user.ID, user.Email, h.cfg.JWT.RefreshTokenSecret, h.cfg.JWT.RefreshTokenExpiry)
-	if err != nil {
-		response.InternalServerError(c, "Failed to generate refresh token")
-		return
-	}
-
-	response.OK(c, models.LoginResponse{
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
-		User:         *user,
-	})
-
 }
---
+++ internal/handlers/user_handler.go
@@ -0,0 +1,87 @@
+package handlers
+
+import (
+	"database/sql"
+	"errors"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/muhammedshamil8/url-shortener/internal/response"
+)
+
+// GetProfile godoc
+//
+//	@Summary	Get profile
+//	@Description	Get profile
+//	@Tags	Users
+//	@Produce	json
+//	@Success	200	{object}	models.SuccessResponse
+//	@Failure	404	{object}	models.ErrorResponse
+//	@Failure	500	{object}	models.ErrorResponse
+//	@Router	/me [get]
+func (h *Handler) GetProfileHandler(c *gin.Context) {
+	email := c.GetString("email")
+	user, err := h.repo.GetUserByEmail(email)
+	if err != nil {
+		response.InternalServerError(c, "Failed to get user")
+		return
+	}
+	response.OK(c, gin.H{
+		"user": user,
+	})
+}
+
+// ListUserURLs godoc
+//
+//	@Summary	List user URLs
+//	@Description	List user URLs
+//	@Tags	Users
+//	@Produce	json
+//	@Success	200	{object}	models.SuccessResponse
+//	@Failure	404	{object}	models.ErrorResponse
+//	@Failure	500	{object}	models.ErrorResponse
+//	@Router	/me/urls [get]
+func (h *Handler) ListUserURLs(c *gin.Context) {
+	email := c.GetString("email")
+	urls, err := h.repo.GetAllURLsByUserEmail(email)
+	if err != nil {
+		response.InternalServerError(c, "Failed to get user urls")
+		return
+	}
+	response.OK(c, gin.H{
+		"urls": urls,
+	})
+}
+
+// DeleteURL godoc
+//
+//	@Summary	Delete URL
+//	@Description	Delete URL
+//	@Tags	Users
+//	@Produce	json
+//	@Param	id	path	int	true	"URL ID"
+//	@Success	200	{object}	models.SuccessResponse
+//	@Failure	400	{object}	models.ErrorResponse
+//	@Failure	404	{object}	models.ErrorResponse
+//	@Failure	500	{object}	models.ErrorResponse
+//	@Router	/me/urls/{id} [delete]
+func (h *Handler) DeleteURL(c *gin.Context) {
+	idstr := c.Param("id")
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		response.BadRequest(c, "Invalid id")
+		return
+	}
+	err = h.repo.DeleteUserURL(id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			response.NotFound(c, "URL not found")
+			return
+		}
+		response.InternalServerError(c, "Failed to delete url")
+		return
+	}
+	response.OK(c, gin.H{
+		"message": "URL deleted successfully",
+	})
+}
